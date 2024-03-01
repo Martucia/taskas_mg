6e0efaa5380e4f0fa1c0891fe10178763301c5bd
Martucia <imarta.shlapak@gmail.com>
2024-03-01 19:44:35 +0200

    fix entities

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -18,7 +18,9 @@ export default function HomePage() {
   return (
     <main className="w-full flex flex-col items-center h-full justify-center pt-[30vh]">
       <UiTitle className="text-3xl">Has no category yet</UiTitle>
-      <div className="mt-2 text-slate-500 text-xl">Don't want to add more?</div>
+      <div className="mt-2 text-slate-500 text-xl">
+        Don&apos;t want to add more?
+      </div>
       <AddCategoryButton variant="primary" className="mt-4" />
     </main>
   );
