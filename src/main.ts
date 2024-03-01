489a4d174347a31d709ae7e6568b4c34cc77c828
Martucia <imarta.shlapak@gmail.com>
2024-03-02 00:01:07 +0200

    eh cors 4

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,12 @@ async function bootstrap() {
   //   credentials: true,
   // }
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
