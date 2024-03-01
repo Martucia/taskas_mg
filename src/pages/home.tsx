5d16c25af5308beeb0ec964a17ba0f1e45aeeef9
Martucia <imarta.shlapak@gmail.com>
2024-03-01 23:13:59 +0200

    fix unescaped entities

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
     <main className="w-full flex flex-col items-center h-full justify-center pt-[30vh]">
       <UiTitle className="text-3xl">Has no category yet</UiTitle>
       <div className="mt-2 text-slate-500 text-xl">
-        Don&apos;t want to add more?
+        Dont want to add more?
       </div>
       <AddCategoryButton variant="primary" className="mt-4" />
     </main>
